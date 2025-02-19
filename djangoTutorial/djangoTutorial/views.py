8d775a0e44827458ec8b0103a9f21f7ae3e92cbd
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-02-19 12:54:13 +0500

    basic django project creation. styles added.

---
+++ djangoTutorial/djangoTutorial/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, 'website/index.html')
+
+def about(request):
+    return HttpResponse("This is the about page.")
+
+def contact(request):
+    return HttpResponse("Contact us at contact@example.com.")
