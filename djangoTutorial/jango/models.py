0bb4dce76eda35c7288bc638837959a4431db3ff
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-02-19 16:15:26 +0500

    creating database tables

--- djangoTutorial/jango/models.py
+++ djangoTutorial/jango/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.utils import timezone
+
 
 # Create your models here.
+class ChaiVariety(models.Model):
+    CHAI_TYPE_CHOICES = [
+        ('ML', 'Masala'),
+        ('GR', 'Ginger'),
+        ('KL', 'Kiwi'),
+    ]
+    name = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='chai_images/')
+    type = models.CharField(max_length=2, choices=CHAI_TYPE_CHOICES)
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.name} - {self.get_type_display()} - Added on {self.date_added.strftime('%Y-%m-%d')}"
--- djangoTutorial/jango/views.py
+++ djangoTutorial/jango/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import ChaiVariety
 
 # Create your views here.
 def jango(request):
-    return render(request, 'jango/all_tutorial.html')
+    chais = ChaiVariety.objects.all()
+    return render(request, 'jango/all_tutorial.html', {'chais': chais})
