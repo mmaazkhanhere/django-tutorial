e144d61109892405e78d3c1bbef3da71316a637e
Mian Maaz Ullah Khan <115078151+mmaazkhanhere@users.noreply.github.com>
2025-02-19 16:35:01 +0500

    relational models and tables added

--- djangoTutorial/jango/admin.py
+++ djangoTutorial/jango/admin.py
@@ -1,5 +1,22 @@
 from django.contrib import admin
-from .models import ChaiVariety
+from .models import ChaiVariety, ChaiReviews, Store, ChaiCertificate
+
+class ChaiReviewInLine(admin.TabularInline):
+    model = ChaiReviews
+    extra = 2 # you can see two fields to add Chai
+
+class ChaiVarietyAdmin(admin.ModelAdmin):
+    list_display = ('name', 'type', 'date_added')
+    inlines = [ChaiReviewInLine]
+
+class StoreAdmin(admin.ModelAdmin):
+    list_display = ('name', 'location')
+    filter_horizontal = ('chai_varieties',)
+
+class ChaiCertificateAdmin(admin.ModelAdmin):
+    list_display = ('chai', 'certificate_number')
 
 # Register your models here.
-admin.site.register(ChaiVariety)
+admin.site.register(ChaiVariety, ChaiVarietyAdmin)
+admin.site.register(Store, StoreAdmin)
+admin.site.register(ChaiCertificate, ChaiCertificateAdmin)
--- djangoTutorial/jango/models.py
+++ djangoTutorial/jango/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 
 # Create your models here.
@@ -16,3 +17,38 @@ class ChaiVariety(models.Model):
 
     def __str__(self):
         return f"{self.name} - {self.get_type_display()} - Added on {self.date_added.strftime('%Y-%m-%d')}"
+
+# One to Many Model
+
+class ChaiReviews(models.Model):
+    chai = models.ForeignKey(ChaiVariety, on_delete=models.CASCADE, related_name='reviews')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    rating = models.IntegerField()
+    comment = models.TextField() 
+    date_added = models.DateTimeField(default=timezone.now)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.chai.name} - {self.rating} stars"
+    
+
+# Many to Many Model
+
+class Store(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    chai_varieties = models.ManyToManyField(ChaiVariety, related_name='stores')
+
+    def __str__(self):
+        return self.name
+    
+
+# One to One Model
+
+class ChaiCertificate(models.Model):
+    chai = models.OneToOneField(ChaiVariety, on_delete=models.CASCADE, related_name='certificate')
+    certificate_number = models.CharField(max_length=20, unique=True)
+    issue_date = models.DateField(default=timezone.now)
+    valid_until = models.DateField()
+
+    def __str__(self):
+        return f"Certificate for {self.chai.name} - {self.certificate_number}"
